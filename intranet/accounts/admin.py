bf60a59436d992e2fe75eeabd6c741af06183e4a
Eric Abruzzese <eric.abruzzese@gmail.com>
2022-12-10 19:04:29 -0500

    Implement a custom user model

--- intranet/accounts/admin.py
+++ intranet/accounts/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """A Django admin configuration for Users."""
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
--- intranet/accounts/models.py
+++ intranet/accounts/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     """An intranet user."""
+
+    email = models.EmailField(
+        verbose_name=_("email address"),
+        max_length=255,
+        unique=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username",)
