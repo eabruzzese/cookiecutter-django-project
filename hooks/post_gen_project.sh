8b8e020be0423583cead18d5ce6f700955c11210
Eric Abruzzese <eric.abruzzese@aptible.com>
2026-05-17 18:47:03 -0400

    Install the prek hooks on project init

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,5 +10,13 @@ uv sync
 
 # Initialize the git repository
 git init .
+
+# Install the prek hooks
+uv run prek install -f
+
+# Run the prek hooks
+uv run prek run --all-files
+
+# Make the initial commit
 git add --all
 git commit -m "Initial commit"
